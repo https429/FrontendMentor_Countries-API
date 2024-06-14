fc220de77e20230c45f80c01c84c0eba2db65fa6
Harm Meyer-Borstel <harm.meyer-borstel@bredex.de>
2024-06-14 16:09:42 +0200

    Added Country Service and rough sketch of overview

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CountryDTO, CountryService, Region} from "../service/country.service";
+import {Observable} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  regionList$!: Observable<Region[]>
+  countryList$!: Observable<CountryDTO[]>
 
+  constructor(private countryService: CountryService) {
+  }
+
+  ngOnInit(): void {
+    this.regionList$ = this.countryService.getAllRegions()
+    this.countryList$ = this.countryService.getAllCountries()
+  }
 }
---
+++ src/app/service/country.service.ts
@@ -0,0 +1,67 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+
+export type Region = string;
+export type CountryDTO = {
+  name: {
+    common: string
+    official: string
+  }
+  tld: string[]
+  region: string
+  subregion: string
+  languages: string[]
+  capital: string[]
+  latlng: [number, number]
+  landlocked: boolean
+  area: number
+  maps: string[]
+  population: number
+  timezones: string[]
+  flags: {
+    png: string
+    svg: string
+  }
+  startOfWeek: string
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+  private readonly BASE_URL: string = "https://restcountries.com/v3.1"
+  private readonly FIELDS: string[] = [
+    'name', 'tld', 'region', 'capital', 'latlng', 'area', 'maps', 'population', 'timezones', 'flags', 'startOfWeek',
+    'country', 'flags'
+  ];
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  getAllRegions(): Observable<Region[]> {
+    return this.http
+      .get<{ region: string }[]>(
+        `${this.BASE_URL}/all`, {
+          params: {fields: 'region'}
+        })
+      .pipe(
+        map(response => response.map(item => item.region)),
+        map(regions => Array.from(new Set(regions))),
+        map(regions => regions.sort())
+      )
+  }
+
+  getAllCountries(): Observable<CountryDTO[]> {
+    return this.http
+      .get<CountryDTO[]>(
+        `${this.BASE_URL}/all`, {
+          params: {fields: this.FIELDS.join(',')}
+        })
+      .pipe(
+        map(countryList => countryList.sort((a, b) => a.name.common.localeCompare(b.name.common))),
+      )
+  }
+}
