f2ecf5d1575e943b917b7c8ce95135e70da786b8
Harm Meyer-Borstel <harm.meyer-borstel@bredex.de>
2024-07-02 13:42:46 +0200

    Bug Fix in DeepLink

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -41,32 +41,24 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    // Load Regions Select
+    // Build Form
+    this.filterForm = this.fb.group({
+      searchInput: [''],
+      regionSelect: [this.NOT_SELECTED]
+    })
+    // Init Subscriptions
+    this.initFormChangeListeners()
+    this.initUpdateFormByQueryParams()
+    // Define Observables
     this.regionList$ = this.countryService.getAllRegions()
-    this.setUpForm()
-    this.initFormByQueryParams()
+    this.initCountryListObservable()
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach(sub => sub.unsubscribe())
   }
 
-  private setUpForm(): void {
-    // Build Form
-    this.filterForm = this.fb.group({
-      searchInput: [''],
-      regionSelect: [this.NOT_SELECTED]
-    })
-
-    // Update Query Params if form changes
-    this.subscriptions.push(
-      this.filterForm.valueChanges
-        .pipe(
-          throttle(() => interval(1000), {leading: false, trailing: true}),
-        )
-        .subscribe(this.updateQueryParams.bind(this))
-    )
-
+  private initCountryListObservable(): void {
     // Connect CountryService Observable to Component & Configure Filter Pipes
     this.countryList$ = this.regionSelect.valueChanges
       .pipe(
@@ -87,43 +79,65 @@ export class HomeComponent implements OnInit, OnDestroy {
               map(search => {
                 const searchStr = search.trim()
                 return searchStr.length > 0
-                  ? countryList.filter(country => country.name.common.includes(searchStr))
+                  ? this.filterCountries(searchStr, countryList)
                   : countryList
-              })
-            )
-        )
-      )
-
-    // Create Subscriptions to get notified if Input Value Change & Activate Reset Button
-    this.subscriptions.push(
-      this.searchInput.valueChanges.subscribe(value =>
-        this.searchInputClearState = value ? 'active' : 'inactive'
-      ))
-    this.subscriptions.push(
-      this.regionSelect.valueChanges.subscribe(value =>
-        this.regionSelectClearState = value != this.NOT_SELECTED ? 'active' : 'inactive'
-      ))
+              }))))
   }
 
-  private initFormByQueryParams(): void {
-    const searchVal: string | undefined = this.route.snapshot.queryParamMap.get(this.SEARCH_QUERY_PARAM)
-      ?.trim().toLowerCase()
-    const regionVal: string | undefined = this.route.snapshot.queryParamMap.get(this.REGION_QUERY_PARAM)
-      ?.trim().toLowerCase()
+  private initFormChangeListeners(): void {
+    this.subscriptions.push(
+      this.filterForm.valueChanges
+        .pipe(
+          throttle(() => interval(1000), {leading: false, trailing: true}))
+        .subscribe(this.updateQueryParams.bind(this)),
+      // Reset Buttons on Input & Select Field
+      this.searchInput.valueChanges.subscribe(value =>
+        this.searchInputClearState = value ? 'active' : 'inactive'),
+      this.regionSelect.valueChanges.subscribe(value =>
+        this.regionSelectClearState = value != this.NOT_SELECTED ? 'active' : 'inactive'),
+    )
+  }
 
-    searchVal && this.searchInput.setValue(searchVal)
-    regionVal && this.regionSelect.setValue(regionVal)
+  private initUpdateFormByQueryParams(): void {
+    this.subscriptions.push(
+      this.route.queryParamMap.subscribe(map => {
+        // Returns parsed Query Param or Default Value
+        const searchVal: string = (map.get(this.SEARCH_QUERY_PARAM)?.trim().toLowerCase()) || ''
+        const regionVal: string = (map.get(this.REGION_QUERY_PARAM)?.trim().toLowerCase()) || this.NOT_SELECTED
+        // Load and parse current form Values
+        const currentSearchVal = this.searchInput.value.trim().toLowerCase()
+        const currentRegionVal = this.regionSelect.value.trim().toLowerCase()
+        // Only update the Form, if the query Value differs
+        currentSearchVal !== searchVal && this.searchInput.setValue(searchVal)
+        currentRegionVal !== regionVal && this.regionSelect.setValue(regionVal)
+      }))
   }
 
   private updateQueryParams(): void {
+    const queryParams: any = {}
+    // Put trimmed and lowered filter strings into query
+    if (this.searchInput.value) {
+      queryParams[this.SEARCH_QUERY_PARAM] = this.searchInput.value.trim().toLowerCase()
+    }
+    if (this.regionSelect.value && this.regionSelect.value != this.NOT_SELECTED) {
+      queryParams[this.REGION_QUERY_PARAM] = this.regionSelect.value.trim().toLowerCase()
+    }
+
     this.router.navigate([], {
       relativeTo: this.route,
       preserveFragment: true,
-      queryParams: {
-        ...(this.searchInput.value && {[this.SEARCH_QUERY_PARAM]: this.searchInput.value}),
-        ...(this.regionSelect.value && this.regionSelect.value != this.NOT_SELECTED && {[this.REGION_QUERY_PARAM]: this.regionSelect.value}),
-      },
+      queryParams: queryParams,
     })
+  }
+
+  private filterCountries(searchStr: string, countryList: CountryDTO[]): CountryDTO[] {
+    return countryList.filter(country => {
+        const countryName = country.name.common.toLowerCase()
+        searchStr = searchStr.trim().toLowerCase()
+
+        return countryName.includes(searchStr)
+      }
+    )
   }
 
   resetSearchInput(): void {
